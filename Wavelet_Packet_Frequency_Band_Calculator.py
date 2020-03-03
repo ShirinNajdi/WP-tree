d098410e6e3c1a3031231789c5a7d149d41f7f4e
Ali Gharbali <30293430+AliGharbali@users.noreply.github.com>
2020-03-03 16:13:20 +0100

    Update the description

--- Wavelet_Packet_Frequency_Band_Calculator.py
+++ Wavelet_Packet_Frequency_Band_Calculator.py
@@ -1,4 +1,7 @@
-
+"""
+This repo will help researchers to explore sub-bands of wavelet packet decomposition and visualize them. 
+This will lead to choose correct sub-bands for feature extraction.
+"""
 import itertools as it
 import numpy as np
 import matplotlib.pyplot as plt
